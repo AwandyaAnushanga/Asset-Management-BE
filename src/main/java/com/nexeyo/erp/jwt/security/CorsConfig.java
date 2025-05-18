3c8e4e8d7828dd7bbbda2450b0ad4a20eb3f73ef
IT22356154 <127285892+IT22356154@users.noreply.github.com>
2025-05-18 15:07:36 +0530

    cors config

--- src/main/java/com/nexeyo/erp/jwt/security/CorsConfig.java
+++ src/main/java/com/nexeyo/erp/jwt/security/CorsConfig.java
@@ -1,19 +1,19 @@
-package com.nexeyo.erp.jwt.security;//package com.nexeyo.erp.jwt.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer { // Implement WebMvcConfigurer
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // Allow all endpoints
-//                .allowedOrigins("https://firestore.life") // Allow only this domain
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.nexeyo.erp.jwt.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer { // Implement WebMvcConfigurer
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:3000") // Allow only this domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
