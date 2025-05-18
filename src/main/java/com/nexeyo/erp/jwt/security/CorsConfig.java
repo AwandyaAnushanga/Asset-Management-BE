963549fd9fac0eb045f3382dbe82cd6abd011043
IT22356154 <127285892+IT22356154@users.noreply.github.com>
2025-05-18 15:56:57 +0530

    frontend port

--- src/main/java/com/nexeyo/erp/jwt/security/CorsConfig.java
+++ src/main/java/com/nexeyo/erp/jwt/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer { // Implement WebMvcConfigu
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow only this domain
+                .allowedOrigins("http://localhost:8080") // Allow only this domain
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
